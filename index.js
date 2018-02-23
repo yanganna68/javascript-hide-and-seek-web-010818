fdd767180db7d35b4336a70366f45cd91bfdec6a
Anna Yang <yanganna68@gmail.com>
2018-02-23 12:38:07 -0500

    Done.

---
+++ index.js
@@ -0,0 +1,19 @@
+function getFirstSelector(selector){
+  return document.querySelector(selector);
+}
+
+function nestedTarget(){
+  return document.querySelector('#nested .target')
+}
+
+function deepestChild(){
+  return document.querySelector('#grand-node div div div div')
+}
+
+function increaseRankBy(n){
+  const lis = document.getElementById('app').querySelectorAll('ul.ranked-list li')
+
+for (let i = 0; i < lis.length; i++) {
+  lis[i].innerHTML = (parseInt(lis[i].innerHTML) + n).toString()
+}
+}
